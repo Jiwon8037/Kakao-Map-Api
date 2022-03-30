6d09d9331187ebe4a7f2108d5a67b984935b6170
Jiwon8037 <lego2090@naver.com>
2022-03-30 23:39:25 +0900

    좌표로 지도 이동

--- index.js
+++ index.js
@@ -6,34 +6,36 @@ const port = 8080
 const server=http.createServer(app)
 const fs = require('fs')
 
+const apiUrl= 'https://dapi.kakao.com/v2/local/search/keyword.json?query=';
+const apiKey= {'Authorization':'KakaoAK 카카오api키'};
+let kakaoData;
+let serchKeyword='';
+
 app.use(express.json());
 app.use(express.static('./'));
 
 app.get('/',(req,res)=>{
     fs.readFile('/index.html',(err,data)=>{
-        res.writeHead(200, {'Content-Type':'text/html'});
+        res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
         res.end();
     })
-   
 });
 
-let serchKeyword='안산';
 app.post('/',(req,res,next)=>{
     serchKeyword=String(req.body.keyword);
-    //console.log(req.body)
-    console.log(serchKeyword)
+
+    axios.get(apiUrl+encodeURI(serchKeyword), {headers:apiKey},)
+    .then(function(response) {
+            kakaoData=response.data.documents;
+            console.log(kakaoData[0]);
+            sendData={
+                x:kakaoData[0].x,
+                y:kakaoData[0].y,
+            }
+            res.json(sendData);
+        }
+    );
 });
-
-
-    const apiUrl='https://dapi.kakao.com/v2/local/search/keyword.json?query='+encodeURI(serchKeyword);
-
-    axios.get(apiUrl,{headers:{'Authorization':'KakaoAK 카카오api키'}},)
-    .then(
-        function(response) { 
-            console.log(JSON.stringify(response.data));
-    });
-
-
 
 server.listen(port, ()=> {
     console.log(port+'포트로 서버 실행');
